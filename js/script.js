a765058ad94b09f878d63668bc7349c8ca609ea6
Toye Ogundepo <ogundepo@gmail.com>
2021-01-16 16:00:49 -0600

    Project Enhancements
    Attempting to achieve "Exceeds Expecations." Added random background color, setInterval function, and tags property.

--- js/script.js
+++ js/script.js
@@ -13,27 +13,30 @@ project 1 - A Random Quote Generator
 const quotes = [
   {
     quote: "Imagination is more important than knowledge.",
-    source: "Albert Einstein"
+    source: "Albert Einstein",
   },
   {
     quote: "I will not lose, for even in defeat, there’s a valuable lesson learned, so it evens up for me.",
-    source: "Shawn Carter"
+    source: "Shawn Carter",
   },
   {
     quote: "If you really look closely, most overnight successes took a long time.",
-    source: "Steve Jobs"
+    source: "Steve Jobs",
+    tags: "Business"
   },
   {
     quote: "If I have seen further, it is by standing upon the shoulders of giants.",
     source: "Sir Isaac Newton",
     citation: "Letter written to fellow scientist, Robert Hooke",
-    year: 1675
+    year: 1675,
+    tags: "Science"
   },
   {
     quote: "This ain't no tall order, this is nothing to me. Difficult takes a day, impossible takes a week.",
     source: "JAY-Z",
     citation: "Diamonds Remix",
-    year: 2005
+    year: 2005,
+    tags: "Perseverence"
   },
 ];
 
@@ -50,26 +53,42 @@ function getRandomQuote(x) {
 }
 // getRandomQuote(quotes);
 
+function randomBackgroundColor() {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  return `${r}, ${g}, ${b}`
+}
+
 
 /***
  * The `printQuote` function prints the contents of the object (quote, source, citation, and year) to the page.
  * Since the year and citation keys are optional, the `printQuote` function contains a conditional to handle whether
  * or not the object contains a year or citation.
+ * 
+ * The body variable was added in order to change background color
 ***/
+const body = document.querySelector("body");
+
 function printQuote() {
   const retrievedRandomQuote = getRandomQuote(quotes);
-  console.log(retrievedRandomQuote)
+  let newBackgroundColor = randomBackgroundColor();
+  body.style.backgroundColor = `rgb(${newBackgroundColor})`;
 
   let html = `
     <p class="quote">${retrievedRandomQuote.quote}</p>
-    <p class="quote">${retrievedRandomQuote.source}`
+    <p class="source">${retrievedRandomQuote.source}`
 
   if ("citation" in retrievedRandomQuote) {
     html += `<span class="citation">${retrievedRandomQuote.citation}</span>`
   }
 
-  if ("citation" in retrievedRandomQuote) {
+  if ("year" in retrievedRandomQuote) {
     html += `<span class="year">${retrievedRandomQuote.year}</span>`
+  }
+
+  if ("tags" in retrievedRandomQuote) {
+    html += `<span class="tags"> -${retrievedRandomQuote.tags}</span>`
   }
   `</p>
 
@@ -78,6 +97,10 @@ function printQuote() {
   return html;
 }
 
+// setInterval function calls printQuote function every 10 seconds
+setInterval(function() {
+  printQuote()
+}, 10000)
 
 /***
  * click event listener for the print quote button
